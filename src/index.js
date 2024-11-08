c6f7de5c6510adcaeb9ecfe50c3fbcb417b314d5
Joe Bush <joebush4444@gmail.com>
2024-11-08 12:12:07 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector("form")
+  inputForm.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    const inputValue = e.target.children[1].value;
+    console.log(inputValue);
+      fetch("http://localhost:3000/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+  });
+});}
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', init)
